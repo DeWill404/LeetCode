c4d0ccea2fb7fca9d1483bc4e6629d3671dbd6dc
DeWill404 <mr.irshad.siddique@gmail.com>
2020-10-17 21:30:44 +0530

    Nim Game

---
+++ Solution/Nim Game.md
@@ -0,0 +1,14 @@
+# Python
+```python
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        return n%4!=0
+```
+# Java
+```java
+class Solution {
+    public boolean canWinNim(int n) {
+        return n%4!=0;
+    }
+}
+```
--- touch.sh
+++ touch.sh
@@ -1,17 +1,21 @@
 # File Loacation Variable
-file="Solution/"$1
-
-
-# Open file in Currently opened window
-code -r "$file"
+file=Solution/$1
 
 
 # Import template in file
 echo "# Python
 \`\`\`python
-
+$(cat template.py)
 \`\`\`
 # Java
 \`\`\`java
-
+$(cat template.java)
 \`\`\`" > "$file"
+
+
+# Replace space with tab
+sed -i 's/    /\t/g' $file
+
+
+# Open file in Currently opened window
+code -r "$file"

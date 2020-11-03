1e0d0a4a91293fa2d427fb99dd8187d32f89c0cd
DeWill404 <mr.irshad.siddique@gmail.com>
2020-11-03 15:40:26 +0530

    Script File Updated

--- touch.sh
+++ touch.sh
@@ -1,4 +1,4 @@
-# File Loacation Variable
+# # File Loacation Variable
 file=Solution/$1".md"
 
 
@@ -19,3 +19,39 @@ sed -i 's/    /\t/g' "$file"
 
 # Open file in Currently opened window
 code -r "$file"
+
+
+# Address of new File
+new_file="[$1](https://github.com/DeWill404/LeetCode/blob/master/Solution/${1// /%20}.md) |"
+
+
+# Iterate i to table divider line "|-|-|-|-|-|"
+i=1
+while [[ '|-|-|-|-|-|' != $(sed -n "$i"p "README.md") ]]
+do
+	i=$[$i+1]
+done
+# Move one line below divider, to get first row
+i=$[$i+1]
+
+# Iterate to last row of table
+while [ $(awk -F"|" "{print NF-1}" <<< $(sed -n "$i"p "README.md")) -eq 6 ]
+do
+	i=$[$i+1]
+done
+
+
+# Get last line details
+line=$(sed -n "$i"p "README.md")
+if [[ $line == "" ]]; then
+	# Add new line
+	i=$[$i-1]
+	new_line=$(sed $i'a| '"$new_file" "README.md")
+else
+	# Append new address to previous
+	new_line=$(replace "$line" "$line $new_file" < README.md)
+fi
+
+
+# Update new address in README.md file
+echo "$new_line" > README.md
